be956ec86d6b0f3d775ea0e89950d9b0fbdde442
mrWezyr <38501324+mrWezyr@users.noreply.github.com>
2023-02-25 18:57:48 +0100

    Updated Movement System

--- Assets/Scrips/Movement.cs
+++ Assets/Scrips/Movement.cs
@@ -19,27 +19,15 @@ public class Movement : MonoBehaviour
 
     private void Start()
     {
-        if (!head)
-        {
-            Debug.LogError("No head transform - LookAt disabled");
-            enabled = false;
-            return;
-        }
+     
 
         anim = GetComponent<Animator>();
-        lookAtPosition = head.position + transform.forward;
+        
     }
 
     private void Update()
     {
-        Vector3 movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
-        if (movementDirection != Vector3.zero)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(movementDirection);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        }
-
+   
         Turn();
         Move();
     }
@@ -47,34 +35,19 @@ public class Movement : MonoBehaviour
     private void Turn()
     {
         float turnInput = Input.GetAxis("Horizontal");
-        turnInput = Mathf.Lerp(anim.GetFloat("velx"), turnInput, Time.deltaTime * 3f);
+        turnInput = Mathf.Lerp(anim.GetFloat("velx"), turnInput, Time.deltaTime * 2f);
         anim.SetFloat("velx", turnInput);
     }
 
     private void Move()
     {
         float moveInput = Input.GetAxis("Vertical");
-        moveInput = Mathf.Lerp(anim.GetFloat("vely"), moveInput, Time.deltaTime * 2f);
+        moveInput = Mathf.Lerp(anim.GetFloat("vely"), moveInput, Time.deltaTime * 1f);
         anim.SetFloat("vely", moveInput);
       
         bool isMoving = Mathf.Abs(moveInput) > 0.01f;
         anim.SetBool("move", isMoving);
     }
 
-    private void OnAnimatorIK()
-    {
-        lookAtPosition = head.position + transform.forward;
-        float lookAtTargetWeight = looking ? 1.0f : 0.0f;
 
-        Vector3 curDir = transform.forward;
-        Vector3 futDir = lookAtPosition - head.position;
-
-        curDir = Vector3.RotateTowards(curDir, futDir, 6.28f * Time.deltaTime, float.PositiveInfinity);
-        lookAtPosition = head.position + curDir;
-
-        float blendTime = lookAtTargetWeight > lookAtWeight ? lookAtHeatTime : lookAtCoolTime;
-        lookAtWeight = Mathf.MoveTowards(lookAtWeight, lookAtTargetWeight, Time.deltaTime / blendTime);
-        anim.SetLookAtWeight(lookAtWeight, 0.2f, 0.5f, 0.7f, 0.5f);
-        anim.SetLookAtPosition(lookAtPosition);
-    }
 }
